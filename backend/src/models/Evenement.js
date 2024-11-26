e5a8726ddd0161f818656b0013522f45778e07ad
hanane987 <aminahanane987@gmail.com>
2024-11-26 12:03:08 +0100

    Feat(SE-3):Adding the model for the events

---
+++ backend/src/models/Evenement.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const evenementSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', 
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Evenement = mongoose.model('Evenement', evenementSchema);
+export default Evenement;
